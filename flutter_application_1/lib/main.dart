7f34e32ce4a489201c268ef211facf1decbac8f6
Dylan wichman <dylan@bulletproofinteractive.com>
2021-02-22 11:53:56 -0600

    Firebase intitialization on app start implemented

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,18 +1,42 @@
 import 'package:flutter/material.dart';
 
 // Import the firebase_core plugin
+import 'package:firebase_core/firebase_core.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(App());
+}
 
-/// This is the main application widget.
-class MyApp extends StatelessWidget {
-  static const String _title = 'Flutter Code Sample';
+class App extends StatelessWidget {
+  // Create the initialization Future outside of `build`:
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: _title,
-      home: MyStatefulWidget(),
+    return FutureBuilder(
+      // Initialize FlutterFire:
+      future: _initialization,
+      builder: (context, snapshot) {
+        // Check for errors
+        // if (snapshot.hasError) {
+        //   return SomethingWentWrong();
+        // }
+
+        // Once complete, show your application
+        if (snapshot.connectionState == ConnectionState.done) {
+          return MaterialApp(
+            title: "title",
+            home: MyStatefulWidget(),
+          );
+        }
+
+        // TODO: create a loading screen while the app waits to connect to firebase
+        return MaterialApp(
+          title: "title",
+          home: MyStatefulWidget(),
+        );
+      },
     );
   }
 }
@@ -25,7 +49,6 @@ class MyStatefulWidget extends StatefulWidget {
   _MyStatefulWidgetState createState() => _MyStatefulWidgetState();
 }
 
-/// This is the private State class that goes with MyStatefulWidget.
 class _MyStatefulWidgetState extends State<MyStatefulWidget> {
   int _selectedIndex = 0;
   static const TextStyle optionStyle =
