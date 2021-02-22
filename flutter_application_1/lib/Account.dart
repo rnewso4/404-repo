3bfe79f2219a86b1f59c2a3dd00f47ba7cf1d2db
Kyle McCraine <kylemccraine@gmail.com>
2021-02-22 11:11:54 -0600

    Changing Users and Group to be subclasses of Accounts

--- flutter_application_1/lib/Account.dart
+++ flutter_application_1/lib/Account.dart
@@ -1,4 +1,12 @@
 abstract class Account {
+  String _name;
+  String _description;
+
+  Account(String name, String description) {
+    _name = name;
+    _description = description;
+  }
+
   String getName();
   String getDescription();
 }
