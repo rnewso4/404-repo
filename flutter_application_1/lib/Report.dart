29a7970c019d3d73f0c3a58384de003735867d29
George Adler Buras <Adler@Colleens-MacBook-Pro.local>
2021-03-17 11:13:50 -0500

    added enmuerated category variable for Reports

--- flutter_application_1/lib/Report.dart
+++ flutter_application_1/lib/Report.dart
@@ -1,14 +1,26 @@
 import 'User.dart';
 
+enum Category {
+  criminalActivity,
+  suspiciousActivity,
+  badFaithActivity,
+  fakeActivity,
+  other
+}
+
 class Report {
   String _title;
   String _issue;
   User _reporter;
 
-  Report(String title, String issue) {
+  // Specific catagory of report, used to sort by in Firebase
+  var _category;
+
+  Report(String title, String issue, var category) {
     this._title = title;
     this._issue = issue;
     //this._reporter = new User();
+    this._category = category;
   }
 
   String getTitle() {
@@ -17,6 +29,10 @@ class Report {
 
   String getIssue() {
     return _issue;
+  }
+
+  String getCategory() {
+    return _category;
   }
 
   //saves report in firebase
