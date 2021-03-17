0cbc94943fec430a7f27b63bf95374dc122bf19c
Daniel-E-Wise <roaringxtigers@gmail.com>
2021-03-16 21:45:37 -0500

    LatLng for new events from maps

--- flutter_application_1/lib/UI/maps.dart
+++ flutter_application_1/lib/UI/maps.dart
@@ -3,6 +3,7 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:flutter_application_1/services/locator.dart';
 import 'package:flutter_application_1/services/navigation_service.dart';
 import 'package:flutter_application_1/services/route_paths.dart' as routes;
+import 'package:flutter_application_1/UI/newevent.dart';
 import './size_config.dart';
 
 final NavigationService _navigationService = locator<NavigationService>();
@@ -14,6 +15,9 @@ class MapsPage extends StatefulWidget {
 
 class _MapsPageState extends State<MapsPage> {
   List<Marker> myMarker = [];
+  List<Marker> evMarkers = [];
+  bool newEv = false;
+
   @override
   Widget build(BuildContext context) {
     SizeConfig().init(context);
@@ -27,15 +31,18 @@ class _MapsPageState extends State<MapsPage> {
             onTap: _handleTap,
           ),
           SizedBox(
-            height: SizeConfig.blockSizeVertical*9,
+            height: SizeConfig.blockSizeVertical * 9,
             child: GestureDetector(
                 onTap: () {
                   _navigationService.navigateTo(routes.SearchRoute);
                 },
                 behavior: HitTestBehavior.translucent,
                 child: Container(
-                  height: SizeConfig.blockSizeVertical*9,
-                  margin: EdgeInsets.only(left: SizeConfig.blockSizeHorizaontal*14, right: SizeConfig.blockSizeHorizaontal*8, top: SizeConfig.blockSizeVertical*5.3),
+                  height: SizeConfig.blockSizeVertical * 9,
+                  margin: EdgeInsets.only(
+                      left: SizeConfig.blockSizeHorizaontal * 14,
+                      right: SizeConfig.blockSizeHorizaontal * 8,
+                      top: SizeConfig.blockSizeVertical * 5.3),
                   decoration: BoxDecoration(
                     color: Colors.white,
                     border: Border.all(
@@ -46,17 +53,22 @@ class _MapsPageState extends State<MapsPage> {
                   child: Row(
                     children: <Widget>[
                       Container(
-                        padding: EdgeInsets.only(left: SizeConfig.blockSizeHorizaontal*2.3, top: SizeConfig.blockSizeVertical*1.5),
+                        padding: EdgeInsets.only(
+                            left: SizeConfig.blockSizeHorizaontal * 2.3,
+                            top: SizeConfig.blockSizeVertical * 1.5),
                         child: Text(
                           'Q',
                           style: TextStyle(
                               fontFamily: 'bts',
                               color: Color(0xff853DD9),
-                              fontSize: SizeConfig.blockSizeHorizaontal*2.7),
+                              fontSize: SizeConfig.blockSizeHorizaontal * 2.7),
                         ),
                       ),
-                      Text('   Search here', 
-                      style: TextStyle(fontSize: SizeConfig.blockSizeHorizaontal*3.5),)
+                      Text(
+                        '   Search here',
+                        style: TextStyle(
+                            fontSize: SizeConfig.blockSizeHorizaontal * 3.5),
+                      )
                     ],
                   ),
                 )),
@@ -64,16 +76,20 @@ class _MapsPageState extends State<MapsPage> {
           Align(
             alignment: Alignment.bottomRight,
             child: Container(
-              height: SizeConfig.blockSizeVertical*50,
-              padding: EdgeInsets.only(right: SizeConfig.blockSizeHorizaontal*2.3, bottom: SizeConfig.blockSizeVertical*12),
+              height: SizeConfig.blockSizeVertical * 50,
+              padding: EdgeInsets.only(
+                  right: SizeConfig.blockSizeHorizaontal * 2.3,
+                  bottom: SizeConfig.blockSizeVertical * 12),
               child: Align(
                 alignment: Alignment.bottomRight,
                 child: FloatingActionButton(
-                  child: Icon(Icons.add, size: SizeConfig.blockSizeHorizaontal*12),
+                  child: Icon(Icons.add,
+                      size: SizeConfig.blockSizeHorizaontal * 12),
                   elevation: 2.5,
                   backgroundColor: Colors.white,
                   foregroundColor: Color(0xff853DD9),
                   onPressed: () {
+                    _newEvTrue();
                     _navigationService.navigateTo(routes.NewEventRoute);
                   },
                 ),
@@ -85,13 +101,24 @@ class _MapsPageState extends State<MapsPage> {
     );
   }
 
+  void newEvFalse() {
+    newEv = false;
+  }
+
+  void _newEvTrue() {
+    newEv = true;
+  }
+
   _handleTap(LatLng tappedPoint) {
     setState(() {
-      myMarker = [];
-      myMarker.add(Marker(
-        markerId: MarkerId(tappedPoint.toString()),
-        position: tappedPoint,
-      ));
+      if (newEv) {
+        myMarker = [];
+        myMarker.add(Marker(
+          markerId: MarkerId(tappedPoint.toString()),
+          position: tappedPoint,
+        ));
+        updateLatLng(tappedPoint);
+      }
     });
   }
 }
--- flutter_application_1/lib/UI/newevent.dart
+++ flutter_application_1/lib/UI/newevent.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_application_1/UI/size_config.dart';
 import 'package:flutter_application_1/services/locator.dart';
 import 'package:flutter_application_1/services/navigation_service.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:flutter_application_1/UI/maps.dart';
+
 final NavigationService _navigationService = locator<NavigationService>();
 
 final myController1 = TextEditingController();
@@ -9,51 +12,57 @@ final myController2 = TextEditingController();
 final myController3 = TextEditingController();
 final myController4 = TextEditingController();
 
+double _lat; //these can be moved if needed
+double _lng; //these can be moved if needed
+
 class NewEvent extends StatefulWidget {
   @override
   _NewEventState createState() => new _NewEventState();
 }
 
 class _NewEventState extends State<NewEvent> {
-
   //@override
   //void dispose() {
-    // Clean up the controller when the widget is disposed.
-    //myController.dispose();
-    //super.dispose();
- // }
+  // Clean up the controller when the widget is disposed.
+  //myController.dispose();
+  //super.dispose();
+  // }
   @override
   Widget build(BuildContext context) {
     SizeConfig().init(context);
     return Scaffold(
       backgroundColor: Color(0xFFCECECE),
       appBar: AppBar(
-        iconTheme: IconThemeData(color: Colors.black),
-        elevation: 0.0,
-        backgroundColor: Colors.transparent,
-        leading: IconButton(
-          icon: Icon(Icons.arrow_back_ios), 
-          onPressed: () {_navigationService.goBack();} 
-          )
-        ),
+          iconTheme: IconThemeData(color: Colors.black),
+          elevation: 0.0,
+          backgroundColor: Colors.transparent,
+          leading: IconButton(
+              icon: Icon(Icons.arrow_back_ios),
+              onPressed: () {
+                newEvFalse();
+                _navigationService.goBack();
+              })),
       body: SingleChildScrollView(
         child: Column(
           children: <Widget>[
             Textfields('Title', myController1),
-            SizedBox(height: SizeConfig.blockSizeVertical*2.5),
-            Textfields('Location', myController2),
-            SizedBox(height: SizeConfig.blockSizeVertical*2.5),
-            Textfields('Account Type', myController3),
-            SizedBox(height: SizeConfig.blockSizeVertical*2.5),
+            SizedBox(height: SizeConfig.blockSizeVertical * 2.5),
+            Textfields('Start Time', myController2),
+            SizedBox(height: SizeConfig.blockSizeVertical * 2.5),
+            Textfields('End Time', myController2),
+            SizedBox(height: SizeConfig.blockSizeVertical * 2.5),
+            Textfields('Date', myController3),
+            SizedBox(height: SizeConfig.blockSizeVertical * 2.5),
             Textfields('Description', myController4),
             Container(
-              height: SizeConfig.blockSizeVertical*45,
-              padding: EdgeInsets.only(right: SizeConfig.blockSizeHorizaontal*3),
+              height: SizeConfig.blockSizeVertical * 45,
+              padding:
+                  EdgeInsets.only(right: SizeConfig.blockSizeHorizaontal * 3),
               child: Align(
                 alignment: Alignment.bottomRight,
                 child: Container(
-                  height: SizeConfig.blockSizeVertical*5.5,
-                  width: SizeConfig.blockSizeHorizaontal*20,
+                  height: SizeConfig.blockSizeVertical * 5.5,
+                  width: SizeConfig.blockSizeHorizaontal * 20,
                   child: GestureDetector(
                     onTap: () {
                       //print(myController1.text);
@@ -68,7 +77,7 @@ class _NewEventState extends State<NewEvent> {
                           child: Text(
                             'ADD',
                             style: TextStyle(
-                                fontSize: SizeConfig.blockSizeHorizaontal*5,
+                                fontSize: SizeConfig.blockSizeHorizaontal * 5,
                                 color: Color(0xFFEBEBEB),
                                 fontWeight: FontWeight.bold),
                           ),
@@ -84,22 +93,34 @@ class _NewEventState extends State<NewEvent> {
   }
 }
 
+updateLatLng(LatLng position) {
+  String _newLat = '0';
+  String _newLng = '0';
+  var _latNum = double.parse(_newLat);
+  var _lngNum = double.parse(_newLng);
+  assert(_latNum is double);
+  assert(_lngNum is double);
+  _lat = _latNum;
+  _lng = _lngNum;
+}
+
 Textfields(String name, var conc) {
   return Container(
-              height: SizeConfig.blockSizeVertical*7,
-              child: Padding(
-                padding: EdgeInsets.only(left: SizeConfig.blockSizeHorizaontal*4.5, right: SizeConfig.blockSizeHorizaontal*4.5),
-                child: TextField(
-                  controller: conc,
-                    decoration: InputDecoration(
-                        labelText: name,
-                        labelStyle: TextStyle(
-                          color: Color(0xff404040),
-                          fontWeight: FontWeight.bold,
-                          fontSize: SizeConfig.blockSizeHorizaontal*4
-                        ),
-                        focusedBorder: UnderlineInputBorder(
-                            borderSide: BorderSide(color: Color(0xff404040))))),
-              ),
-            );
+    height: SizeConfig.blockSizeVertical * 7,
+    child: Padding(
+      padding: EdgeInsets.only(
+          left: SizeConfig.blockSizeHorizaontal * 4.5,
+          right: SizeConfig.blockSizeHorizaontal * 4.5),
+      child: TextField(
+          controller: conc,
+          decoration: InputDecoration(
+              labelText: name,
+              labelStyle: TextStyle(
+                  color: Color(0xff404040),
+                  fontWeight: FontWeight.bold,
+                  fontSize: SizeConfig.blockSizeHorizaontal * 4),
+              focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(color: Color(0xff404040))))),
+    ),
+  );
 }
