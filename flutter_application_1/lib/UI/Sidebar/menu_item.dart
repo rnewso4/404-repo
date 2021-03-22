8a6d96cb872d73e8c94ece240bd20a387c85fcf7
rnewso4 <rnewso4@lsu.edu>
2021-03-22 13:17:07 -0500

    sidebar added

---
+++ flutter_application_1/lib/UI/Sidebar/menu_item.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/services/locator.dart';
+import 'package:flutter_application_1/services/navigation_service.dart';
+import 'package:flutter_application_1/services/route_paths.dart' as routes;
+
+List listitem = [routes.register, routes.register, routes.register, routes.register, routes.LoginRoute];
+
+final NavigationService _navigationService = locator<NavigationService>();
+
+class MenuItemsPage extends StatelessWidget {
+  
+  final IconData icon;
+  final String title;
+  final Function onTap;
+
+  const MenuItemsPage({Key key, this.icon, this.title, this.onTap}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Row(
+          children: <Widget>[
+            Icon(
+              icon,
+              color: Color(0xffC860FF),
+              size: 30,
+            ),
+            SizedBox(width: 20,),
+            Text(
+              title,
+              style: TextStyle(
+                fontWeight: FontWeight.w300,
+                fontSize: 26,
+                color: Colors.white
+              ),
+            )
+          ],
+        ),
+
+      ),
+    );
+  }
+}
