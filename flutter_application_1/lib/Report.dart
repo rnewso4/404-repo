956d46643d6dee19f955d0422997d0941cef5251
GABuras <geoeagle00@gmail.com>
2021-03-25 01:23:31 -0500

    Added comments to Report and make getCategory functional

--- flutter_application_1/lib/Report.dart
+++ flutter_application_1/lib/Report.dart
@@ -12,14 +12,39 @@ enum Category {
   other
 }
 
+/**
+ * This class is used to represent reports
+ * 
+ * @author
+ * commented by: George Adler Buras
+ */
 class Report {
+  //The title of the report
   String _title;
+
+  //The description of the issue the report is about
   String _issue;
+
+  //The user who created the report
   User _reporter;
 
   //Specific catagory of report, used to sort by in Firebase
   var _category;
 
+  /**
+   * Constructor used to create a report object
+   * 
+   * routine: Report
+   * 
+   * return type: Report
+   * 
+   * parameteres:
+   *    title     [String]    the title of the report
+   *    issue     [String]    the description of the issue the report is about
+   *    category  [var]       the category of the report
+   * 
+   * @author
+   */
   Report(String title, String issue, var category) {
     this._title = title;
     this._issue = issue;
@@ -27,16 +52,56 @@ class Report {
     this._category = category;
   }
 
+  /**
+   * This method gets the title of the report
+   * 
+   * routine: getTitle
+   * 
+   * return type: String
+   * 
+   * @author
+   */
   String getTitle() {
     return _title;
   }
 
+  /**
+   * This method gets the description of the issue the report is about
+   * 
+   * routine: getIssue
+   * 
+   * return type: String
+   * 
+   * @author
+   */
   String getIssue() {
     return _issue;
   }
 
+  /**
+   * This method gets the category of the issue;
+   * transforming it into a String in the process
+   * 
+   * routine: getCategory
+   * 
+   * return type: String
+   * 
+   * @author George Adler Buras
+   */
   String getCategory() {
-    return _category;
+    if (_category == Category.criminalActivity) {
+      return "Criminal Activity";
+    } else if (_category == Category.suspiciousActivity) {
+      return "Suspicious Activity";
+    } else if (_category == Category.badFaithActivity) {
+      return "Bad Faith Activity";
+    } else if (_category == Category.fakeActivity) {
+      return "Fake Activity";
+    } else if (_category == Category.other) {
+      return "Other";
+    } else {
+      return "ERROR: ENUMERATION FAILURE";
+    }
   }
 
   //saves report in firebase
